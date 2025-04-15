f963e98c957e985b17c0d6a0959ec4344449ffaf
aristaanjani17 <iniiristaa@gmail.com>
2025-04-15 21:51:07 +0800

    Keterangan commit

---
+++ Pertemuan6/src/pertemuan6/VolumeBalok.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan6;
+
+/**
+ *
+ * @author Arista Anjani
+ * TGL: 15 April 2025
+ */
+public class VolumeBalok {
+    public int panjang,lebar,tinggi;
+    private int hasil;
+    
+    public int Volume(){
+        this.hasil = this.panjang*this.lebar*this.tinggi;
+        return hasil;
+    }
+    
+            
+    
+}
